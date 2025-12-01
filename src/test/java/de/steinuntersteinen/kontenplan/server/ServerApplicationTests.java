206e1c25ef21254864b6a33e3cc7b44b3ac9c081
Julien Zigan <zigan.julien@gmail.com>
2025-12-01 17:14:35 +0100

    [Init] first commit

---
+++ src/test/java/de/steinuntersteinen/kontenplan/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.steinuntersteinen.kontenplan.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
