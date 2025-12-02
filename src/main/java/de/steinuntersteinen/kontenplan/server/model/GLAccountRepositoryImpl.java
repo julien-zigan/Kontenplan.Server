16456a0f4be79372fd6a87941eb77395541ca20b
Julien Zigan <zigan.julien@gmail.com>
2025-12-02 19:31:39 +0100

    [Feature] implement findAll method for glaccount

--- src/main/java/de/steinuntersteinen/kontenplan/server/model/GLAccountRepositoryImpl.java
+++ src/main/java/de/steinuntersteinen/kontenplan/server/model/GLAccountRepositoryImpl.java
@@ -1,9 +1,12 @@
 package de.steinuntersteinen.kontenplan.server.model;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class GLAccountRepositoryImpl implements GLAccountRepository {
@@ -19,5 +22,12 @@ public class GLAccountRepositoryImpl implements GLAccountRepository {
     @Transactional
     public void save(GLAccount theGLAccount) {
         entityManager.persist(theGLAccount);
+    }
+
+    @Override
+    public List<GLAccount> findAll() {
+        TypedQuery<GLAccount> query = entityManager.createQuery("FROM GLAccount",  GLAccount.class);
+
+        return query.getResultList();
     }
 }
