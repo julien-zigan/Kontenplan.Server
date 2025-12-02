dccd033f7a55c1d92c892871e6176c3303c2e066
Julien Zigan <zigan.julien@gmail.com>
2025-12-02 20:13:51 +0100

    [Test] add serialization test for GLAccount

---
+++ src/test/java/de/steinuntersteinen/kontenplan/server/GLAccountJsonTest.java
@@ -0,0 +1,30 @@
+package de.steinuntersteinen.kontenplan.server;
+
+import de.steinuntersteinen.kontenplan.server.model.GLAccount;
+import de.steinuntersteinen.kontenplan.server.model.GLAccountType;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.json.JsonContent;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@JsonTest
+public class GLAccountJsonTest {
+
+    @Autowired
+    private JacksonTester<GLAccount> json;
+
+    @Test
+    public void GLAccountSerializationTest() throws IOException {
+
+        GLAccount glAccount = new GLAccount("4", GLAccountType.CLASS, "Test", null, null, null);
+
+        JsonContent<GLAccount> actualJson = json.write(glAccount);
+
+        assertThat(actualJson).isStrictlyEqualToJson("expected-gl-account.json");
+    }
+}
